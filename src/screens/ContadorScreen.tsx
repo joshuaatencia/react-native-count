425c6a87ae17d4321f94090d2037689a883069f6
josue david ruiz atencia <joshuaedts@gmail.com>
2022-01-30 10:12:36 -0500

    first commit

---
+++ src/screens/ContadorScreen.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Fab} from '../components/Fab';
+
+export const ContadorScreen = () => {
+  const [contador, setContador] = useState(10);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {contador}</Text>
+
+      <Fab title="+1" position="br" onPress={() => setContador(contador + 1)} />
+      <Fab title="-1" position="bl" onPress={() => setContador(contador - 1)} />
+
+      {/* <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setContador(contador - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    top: -15,
+  },
+});
